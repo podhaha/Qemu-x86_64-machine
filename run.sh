4df08e28590a86e1f7ae423b9e7a82c98064ab3a
podhaha <140906196+podhaha@users.noreply.github.com>
2023-08-13 09:08:41 +0700

    Fix error

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 apt update -y
 echo install qemu-system
-apt install qemu-system &
+apt install qemu-system -y &
 echo download...
 clear
 sleep 3
@@ -21,7 +21,7 @@ sleep 1
 echo complete
 echo install ngrok ....
  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
- echo complete[ngrok]
+ echo complete ngrok
  sleep 1
  echo add disk
 qemu-img create -f qcow2 1.qcow2 20G
@@ -32,7 +32,7 @@ qemu-img create -f qcow2 1.qcow2 20G
  echo save '1.qcow2'
  sleep 1 
  clear
- echo start Ngrok (region=ap)
+ echo start Ngrok region=ap
  ngrok tcp 5901 --region=ap &
 echo start machine 
 qemu-system-x86_64 -m 13G -vga std -cpu SandyBridge-v2 -smp sockets=1,cores=4,threads=2 -device e1000,netdev=n0 -netdev user,id=n0 -soundhw hda -vnc :1 -boot menu=on -machine usb=on -device usb-tablet -enable-kvm -hda 1.qcow2 &
